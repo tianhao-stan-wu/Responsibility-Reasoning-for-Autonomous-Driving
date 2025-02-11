486727c9fa8823f7f9f6f481e605f83fc2a9ecfa
Tianhao Wu <tianhao_wu2020@163.com>
2025-02-11 15:31:13 -0500

    add carla interface

---
+++ carla_python_api.py
@@ -0,0 +1,40 @@
+import carla
+import random
+
+# connect to server
+client = carla.Client('localhost', 2000)
+world = client.get_world()
+
+# select map
+client.load_world('Town05')
+
+# spawn vehicles
+vehicle_blueprints = world.get_blueprint_library().filter('*vehicle*')
+
+spawn_points = world.get_map().get_spawn_points()
+
+# Spawn 50 vehicles randomly distributed throughout the map 
+# for each spawn point, we choose a random vehicle from the blueprint library
+for i in range(0,50):
+    world.try_spawn_actor(random.choice(vehicle_blueprints), random.choice(spawn_points))
+
+ego_vehicle = world.spawn_actor(random.choice(vehicle_blueprints), random.choice(spawn_points))
+
+
+# Create a transform to place the camera on top of the vehicle
+camera_init_trans = carla.Transform(carla.Location(z=1.5))
+
+# We create the camera through a blueprint that defines its properties
+camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
+
+# We spawn the camera and attach it to our ego vehicle
+camera = world.spawn_actor(camera_bp, camera_init_trans, attach_to=ego_vehicle)
+
+for vehicle in world.get_actors().filter('*vehicle*'):
+    vehicle.set_autopilot(True)
+
+ego_bp = world.get_blueprint_library().find('vehicle.lincoln.mkz_2020')
+
+ego_bp.set_attribute('role_name', 'hero')
+
+ego_vehicle = world.spawn_actor(ego_bp, random.choice(spawn_points))
